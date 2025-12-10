e6922af69c80f18b942908a42a883e4b048d952e
rafi1341 <131365258+rafi1341@users.noreply.github.com>
2025-12-11 05:38:13 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ import os
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 
 # DIRECT URL (recommended for now)
-WEB_APP_URL = "https://tokenhatch.onrender.com"
+WEB_APP_URL = "https://tokenhatch.onrender.com/?v=2"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     chat_id = update.effective_chat.id
@@ -26,6 +26,7 @@ if __name__ == "__main__":
     app = ApplicationBuilder().token(BOT_TOKEN).build()
     app.add_handler(CommandHandler("start", start))
     app.run_polling()
+
 
 
 
