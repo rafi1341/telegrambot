9eb6987a1c66f4cfc2581469d55298dd7072568e
rafi1341 <131365258+rafi1341@users.noreply.github.com>
2025-12-10 23:51:38 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,14 +11,15 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     chat_id = update.effective_chat.id
 
     keyboard = [
-        [InlineKeyboardButton("Launch App", web_app=WebAppInfo(url=WEB_APP_URL))]
+        [InlineKeyboardButton("Launch App💵", web_app=WebAppInfo(url=WEB_APP_URL))]
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
 
     await context.bot.send_photo(
         chat_id=chat_id,
         photo=open("banner.png", "rb"),
-        caption="Welcome! Click the button below to open the miniapp.",
+        caption="Welcome to TokenHatch! 🥚
+Hatch creatures, get $EGG crypto points, and earn airdrops!",
         reply_markup=reply_markup
     )
 
@@ -26,3 +27,4 @@ if __name__ == "__main__":
     app = ApplicationBuilder().token(BOT_TOKEN).build()
     app.add_handler(CommandHandler("start", start))
     app.run_polling()
+
